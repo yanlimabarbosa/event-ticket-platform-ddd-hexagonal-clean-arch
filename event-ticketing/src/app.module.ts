0285184470ca21b20dc6ae6a9830d0c6f8ffd3dc
Yan Lima Barbosa <yanbr763@gmail.com>
2026-04-04 15:38:54 -0300

    feate: setup nestjs project

--- event-ticketing/src/app.module.ts
+++ event-ticketing/src/app.module.ts
@@ -1,15 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import mikroOrmConfig from './mikro-orm.config';
+import { MikroOrmModule } from '@mikro-orm/nestjs'
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import mikroOrmConfig from './mikro-orm.config'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MikroOrmModule.forRoot(mikroOrmConfig),
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MikroOrmModule.forRoot(mikroOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })

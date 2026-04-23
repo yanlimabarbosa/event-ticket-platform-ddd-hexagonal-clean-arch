7ca77e9e0e6ecdff0358b76a860caaaa9f9df65a
Yan Lima Barbosa <yanbr763@gmail.com>
2026-04-22 23:26:01 -0300

    refactor(shared): remove old Clock/IdGenerator locations in ordering
    - Deletes the now-duplicated files so the shared versions are the only source

--- event-ticketing/src/ordering/application/ports/out/Clock.ts
+++
@@ -1,3 +0,0 @@
-export abstract class Clock {
-  public abstract now(): Date
-}
--- event-ticketing/src/ordering/infrastructure/out/services/SystemClock.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common'
-import { Clock } from '../../../application/ports/out/Clock'
-
-@Injectable()
-export class SystemClock extends Clock {
-  public override now(): Date {
-    return new Date()
-  }
-}

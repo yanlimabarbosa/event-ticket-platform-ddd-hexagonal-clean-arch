a78ff551452e63c7b5c43e75495c0644a9215b24
Yan Lima Barbosa <yanbr763@gmail.com>
2026-04-22 23:26:26 -0300

    chore(modules): wire SharedInfrastructureModule and outbox publisher
    - AppModule imports SharedInfrastructureModule (Clock, IdGenerator, UnitOfWork)
    - OrderingModule registers OutboxEventEntity with MikroOrmModule.forFeature,
      swaps DomainEventPublisher binding to OutboxDomainEventPublisher, and
      provides FakeTicketTypePricing for the new port

--- event-ticketing/src/app.module.ts
+++ event-ticketing/src/app.module.ts
@@ -1,18 +1,18 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { EventEmitterModule } from '@nestjs/event-emitter'
 import mikroOrmConfig from './mikro-orm.config'
 import { OrderingModule } from './ordering/ordering.module'
+import { SharedInfrastructureModule } from './shared/infrastructure/SharedInfrastructureModule'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MikroOrmModule.forRoot(mikroOrmConfig),
+    EventEmitterModule.forRoot(),
+    SharedInfrastructureModule,
     OrderingModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- event-ticketing/src/ordering/ordering.module.ts
+++ event-ticketing/src/ordering/ordering.module.ts
@@ -1,28 +1,30 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs'
 import { Module } from '@nestjs/common'
+import { DomainEventPublisher } from './application/ports/out/DomainEventPublisher'
+import { EventAvailabilityChecker } from './application/ports/out/EventAvailabilityChecker'
+import { OrderRepository } from './application/ports/out/OrderRepository'
+import { PaymentGateway } from './application/ports/out/PaymentGateway'
+import { TicketTypePricing } from './application/ports/out/TicketTypePricing'
 import { CancelOrderUseCase } from './application/use-cases/CancelOrderUseCase'
 import { CreateOrderUseCase } from './application/use-cases/CreateOrderUseCase'
 import { ExpireOrderUseCase } from './application/use-cases/ExpireOrderUseCase'
 import { ListAttendeeOrdersUseCase } from './application/use-cases/ListAttendeeOrdersUseCase'
 import { PayOrderUseCase } from './application/use-cases/PayOrderUseCase'
-import { Clock } from './application/ports/out/Clock'
-import { EventAvailabilityChecker } from './application/ports/out/EventAvailabilityChecker'
-import { IdGenerator } from './application/ports/out/IdGenerator'
-import { OrderRepository } from './application/ports/out/OrderRepository'
-import { PaymentGateway } from './application/ports/out/PaymentGateway'
-import { CryptoIdGenerator } from './infrastructure/out/services/CryptoIdGenerator'
-import { FakeEventAvailabilityChecker } from './infrastructure/out/services/FakeEventAvailabilityChecker'
-import { FakePaymentGateway } from './infrastructure/out/services/FakePaymentGateway'
-import { SystemClock } from './infrastructure/out/services/SystemClock'
-import { OrdersController } from './infrastructure/in/http/orders/OrdersController'
 import { OrderResponseMapper } from './infrastructure/in/http/orders/mappers/OrderResponseMapper'
+import { OrdersController } from './infrastructure/in/http/orders/OrdersController'
+import { OrderEventLogger } from './infrastructure/in/listeners/OrderEventLogger'
 import { OrderEntity } from './infrastructure/out/persistence/entities/OrderEntity'
 import { OrderItemEntity } from './infrastructure/out/persistence/entities/OrderItemEntity'
+import { OutboxEventEntity } from './infrastructure/out/persistence/entities/OutboxEventEntity'
 import { OrderMapper } from './infrastructure/out/persistence/mappers/OrderMapper'
 import { MikroOrmOrderRepository } from './infrastructure/out/persistence/repositories/MikroOrmOrderRepository'
+import { FakeEventAvailabilityChecker } from './infrastructure/out/services/FakeEventAvailabilityChecker'
+import { FakePaymentGateway } from './infrastructure/out/services/FakePaymentGateway'
+import { FakeTicketTypePricing } from './infrastructure/out/services/FakeTicketTypePricing'
+import { OutboxDomainEventPublisher } from './infrastructure/out/services/OutboxDomainEventPublisher'
 
 @Module({
-  imports: [MikroOrmModule.forFeature([OrderEntity, OrderItemEntity])],
+  imports: [MikroOrmModule.forFeature([OrderEntity, OrderItemEntity, OutboxEventEntity])],
   controllers: [OrdersController],
   providers: [
     CreateOrderUseCase,
@@ -32,11 +34,12 @@ import { MikroOrmOrderRepository } from './infrastructure/out/persistence/reposi
     ListAttendeeOrdersUseCase,
     OrderMapper,
     OrderResponseMapper,
+    OrderEventLogger,
     { provide: OrderRepository, useClass: MikroOrmOrderRepository },
-    { provide: IdGenerator, useClass: CryptoIdGenerator },
-    { provide: Clock, useClass: SystemClock },
     { provide: PaymentGateway, useClass: FakePaymentGateway },
     { provide: EventAvailabilityChecker, useClass: FakeEventAvailabilityChecker },
+    { provide: DomainEventPublisher, useClass: OutboxDomainEventPublisher },
+    { provide: TicketTypePricing, useClass: FakeTicketTypePricing },
   ],
 })
 export class OrderingModule {}
